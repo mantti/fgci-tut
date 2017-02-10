a5aff888116137ef8520adb6a10fff7a738a1b8f
Mika Anttila <mika.anttila@tut.fi>
2017-02-10 12:00:02 +0200

    Enabled root console on idrac serial port

--- roles/local-bios-updates/files/check_idrac_version.sh
+++ roles/local-bios-updates/files/check_idrac_version.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #ipmi-oem dell get-system-info idrac-info |awk -F " : " '/Firmware Version/{print $2}'
-ipmi-oem dell get-system-info idrac-info |awk -F " : " '/Firmware Version/{split($2,AR,/ /) ;print AR[1]}'
+#ipmi-oem dell get-system-info idrac-info |awk -F " : " '/Firmware Version/{split($2,AR,/ /) ;print AR[1]}'
+bmc-info |awk -F": " '/Firmware Revision/{print $2}'
