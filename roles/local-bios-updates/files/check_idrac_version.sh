f6421dc7ced607f1a2fcbd350a616d11ff376ea0
Mika Anttila <mika.anttila@tut.fi>
2017-02-14 11:50:10 +0200

    Moved local-bios-updates so submodule ansible-role-firmware-updates

--- roles/local-bios-updates/files/check_idrac_version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#ipmi-oem dell get-system-info idrac-info |awk -F " : " '/Firmware Version/{print $2}'
-#ipmi-oem dell get-system-info idrac-info |awk -F " : " '/Firmware Version/{split($2,AR,/ /) ;print AR[1]}'
-bmc-info |awk -F": " '/Firmware Revision/{print $2}'
